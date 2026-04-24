98a977272409b387d8712f8290bfa56716731f43
cldavi <cldavifs@gmail.com>
2026-04-24 16:43:52 -0300

    Entidade User configurada

---
+++ src/main/java/com/personalproject/tasklist/users/User.java
@@ -0,0 +1,31 @@
+package com.personalproject.tasklist.users;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+    
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
