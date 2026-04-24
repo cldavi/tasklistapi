2b95baffed31bd687690f3f96a56cc1efa5df15c
cldavi <cldavifs@gmail.com>
2026-04-24 16:44:22 -0300

    Repositório da entidade User configurada

---
+++ src/main/java/com/personalproject/tasklist/users/UserRepository.java
@@ -0,0 +1,11 @@
+package com.personalproject.tasklist.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+}
