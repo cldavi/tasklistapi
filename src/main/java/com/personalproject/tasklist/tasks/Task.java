b83a4efc518588f2ed33dd2d05c230d98e0633d1
cldavii <cldavifs@gmail.com>
2026-04-24 19:36:46 -0300

    Tipo enumerado para o atributo priority da entidade Task configurado

--- src/main/java/com/personalproject/tasklist/tasks/Task.java
+++ src/main/java/com/personalproject/tasklist/tasks/Task.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +28,9 @@ public class Task {
     private String description;
     private LocalDateTime startedAt;
     private LocalDateTime endedAt;
-    private String priority;
+    
+    @Enumerated(value = EnumType.STRING)
+    private TaskPriority priority;
     
     @CreationTimestamp
     private LocalDateTime createdAt;
