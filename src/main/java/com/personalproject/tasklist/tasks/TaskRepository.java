3e191b514f40ea096dfe4f69d4d93c53e74daf02
cldavi <cldavifs@gmail.com>
2026-04-25 23:09:56 -0300

    Método para listar tarefas via ID do usuário

--- src/main/java/com/personalproject/tasklist/tasks/TaskRepository.java
+++ src/main/java/com/personalproject/tasklist/tasks/TaskRepository.java
@@ -1,5 +1,10 @@
 package com.personalproject.tasklist.tasks;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Integer> {}
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    List<Task> findByUserId(UUID id);
+}
